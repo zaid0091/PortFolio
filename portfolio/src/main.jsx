3b08b345d5a90cf31dc3c6838b369446d80d1bde
Zaid Liaqat <zaidliaqat999@gmail.com>
2026-05-08 18:06:01 +0500

    feat: fixed pwa setup error

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// Unregister any stale service workers (leftover from previous PWA setup)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
